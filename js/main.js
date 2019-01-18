238c3da2284e209d2365bcc943f4bfa22307817f
Trelyrien <jskeen@gmail.com>
2019-01-18 14:20:19 -0500

    Minor changes
    I think?

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
+const search = document.getElementById('search');
+const image = document.querySelectorAll('a[data-caption]');
 
-const search = document.getElementById("search");
-const image = document.querySelectorAll("a[data-caption]");
-
-search.addEventListener("keyup", function(event) {
+search.addEventListener('keyup', function (event) {
   let result = search.value.toUpperCase();
-  for (let i=0; i<image.length; i++){
-    let title = image[i].getAttribute("data-caption");
+  for (let i = 0; i < image.length; i++) {
+    let title = image[i].getAttribute('data-caption');
     if (title.toUpperCase().includes(result)) {
       // image[i].style.display = '';
       image[i].classList.add('selected');
-      image[i].classList.remove('not-selected')
+      image[i].classList.remove('not-selected');
     } else {
       // image[i].style.display = 'none';
       image[i].classList.remove('selected');
-      image[i].classList.add('not-selected')
+      image[i].classList.add('not-selected');
     }
   }
 });
